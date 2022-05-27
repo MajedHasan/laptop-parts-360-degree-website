ed0bf1b3eb8e91d120dcd75ee5309ff8340b4411
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-27 10:08:01 +0600

    Home page Banner added

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ import Dashboard from './Pages/Dashboard/Dashboard';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Login/Register';
 import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Routes>
-        <Route to='/' element={<Home></Home>}></Route>
-        <Route to='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route to='/portfolio' element={<MyPortfolio></MyPortfolio>}></Route>
-        <Route to='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/portfolio' element={<MyPortfolio></MyPortfolio>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
 
-        <Route to='/login' element={<Login></Login>}></Route>
-        <Route to='/register' element={<Register></Register>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
 
-        <Route to='*' element={<NotFound></NotFound>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
-      <h2 className='text-purple-500 text-center font-bold text-8xl'>Test</h2>
+      <Footer></Footer>
     </>
   );
 }
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import bgImg from '../../images/home-banner.png'
 
 const Banner = () => {
     return (
-        <div>
-
+        <div className="hero min-h-[85vh]" style={{ background: `url(${bgImg})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+            <div className="hero-overlay bg-opacity-40"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-xl">
+                    <h1 className="mb-5 text-5xl font-bold">Welcome To Parts 360 degree</h1>
+                    <p className="mb-5">We have best quality laptop parts, we are offering the quality parts. We have over million of customer. They are happy to use our products</p>
+                    <button className="btn btn-primary bg-green-500 border-green-300">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 };
