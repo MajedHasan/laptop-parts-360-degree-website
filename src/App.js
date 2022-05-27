4aeac58189f155d94a915c0892dc616d968db5f9
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-27 09:07:17 +0600

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Navbar from './Pages/Shared/Navbar';
 import Home from './Pages/Home/Home';
 import Blogs from './Pages/Blogs/Blogs';
+import MyPortfolio from './Pages/MyPortfolio/MyPortfolio';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -11,6 +16,13 @@ function App() {
       <Routes>
         <Route to='/' element={<Home></Home>}></Route>
         <Route to='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route to='/portfolio' element={<MyPortfolio></MyPortfolio>}></Route>
+        <Route to='/dashboard' element={<Dashboard></Dashboard>}></Route>
+
+        <Route to='/login' element={<Login></Login>}></Route>
+        <Route to='/register' element={<Register></Register>}></Route>
+
+        <Route to='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <h2 className='text-purple-500 text-center font-bold text-8xl'>Test</h2>
     </>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,7 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const CustomLink = ({ to, children, ...props }) => {
+
+        let resolved = useResolvedPath(to)
+        let match = useMatch({ path: resolved.pathname, end: true })
+
+        return <Link to={to}
+            className={match && 'text-white bg-green-400'}
+            {...props}
+        >
+            {
+                children
+            }
+        </Link>
+    }
+
+    const link = <>
+        <li><CustomLink to='/'>Home</CustomLink></li>
+        <li><CustomLink to='/blogs'>Blogs</CustomLink></li>
+        <li><CustomLink to='/portfolio'>Portfolio</CustomLink></li>
+        <li><CustomLink to='/dashboard'>Dashboard</CustomLink></li>
+    </>
+
     return (
         <div className="navbar bg-base-300">
             <div className="navbar-start">
@@ -10,40 +33,23 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='/blog'>Blogs</Link></li>
-                        <li tabIndex="0">
-                            <a className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {
+                            link
+                        }
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <Link to='/' className="btn btn-ghost normal-case text-xl">daisyUI</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex="0">
-                        <a>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {
+                        link
+                    }
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Get started</a>
+                <CustomLink to='/login' style={{ padding: '3px 5px', borderRadius: '4px', marginRight: '6px' }}>Login</CustomLink>
+                <CustomLink to='/register' style={{ padding: '3px 5px', borderRadius: '4px' }}>Register</CustomLink>
             </div>
         </div>
     );
