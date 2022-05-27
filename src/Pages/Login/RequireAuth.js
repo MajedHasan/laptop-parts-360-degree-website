5773fa3bc74d7c5f30e568a385df832c11ea282b
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-27 12:21:26 +0600

    Require Auth added

--- src/Pages/Login/RequireAuth.js
+++ src/Pages/Login/RequireAuth.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import Loading from '../Shared/Loading';
 
-const RequireAuth = () => {
-    return (
-        <div>
+const RequireAuth = ({ children }) => {
 
-        </div>
-    );
+    const location = useLocation()
+    const [user, loading] = useState(false)
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (!user) {
+        return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+    }
+
+    return children;
 };
 
 export default RequireAuth;
