3771e19438c36fab8adf33f4e60922debb4a74df
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-27 08:45:05 +0600

    React Router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import Blogs from './Pages/Blogs/Blogs';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Routes>
+        <Route to='/' element={<Home></Home>}></Route>
+        <Route to='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
       <h2 className='text-purple-500 text-center font-bold text-8xl'>Test</h2>
     </>
   );
