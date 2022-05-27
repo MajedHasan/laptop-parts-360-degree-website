a3c8d1b5cbe230d4a11a73c2c8c4037ced2671f4
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-27 08:38:02 +0600

    Tailwind Css added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <h2 className='text-purple-500 text-center font-bold text-8xl'>Test</h2>
+    </>
   );
 }
 
