3ee76223c0262704c1c3b7dbdb9901a3fe42b178
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-27 12:12:12 +0600

    Home page Business Summary section Font Awesome Icon Added

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -1,10 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFlagCheckered, faUsers, faCheckToSlot, faChartPie } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
 const BusinessSummary = () => {
     return (
         <section className='my-12'>
             <div className="container mx-auto p-4">
-                <div className="top-text mb-6 text-center">
+                <div className="top-text mb-8 text-center">
                     <h2 className='text-4xl text-teal-600 font-bold uppercase'>Thousands Of Business Trust Us</h2>
                     <h3 className='text-2xl text-slate-600 uppercase'>Your Expectation and Our Goal</h3>
                     <div>
@@ -15,18 +17,22 @@ const BusinessSummary = () => {
                 </div>
                 <div className="bottom-content flex flex-wrap justify-between gap-5">
                     <div className='w-full max-w-[100%] md:max-w-[47%] lg:max-w-[22%] text-center'>
+                        <FontAwesomeIcon icon={faFlagCheckered} className="text-5xl text-teal-600 mb-6" />
                         <h2 className='text-4xl font-bold text-slate-800'>05</h2>
                         <p className='text-teal-400 text-xl'>Countries</p>
                     </div>
                     <div className='w-full max-w-[100%] md:max-w-[47%] lg:max-w-[22%] text-center'>
+                        <FontAwesomeIcon icon={faUsers} className="text-5xl text-teal-600 mb-6" />
                         <h2 className='text-4xl font-bold text-slate-800'>1000+</h2>
                         <p className='text-teal-400 text-xl'>Happy Clients</p>
                     </div>
                     <div className='w-full max-w-[100%] md:max-w-[47%] lg:max-w-[22%] text-center'>
+                        <FontAwesomeIcon icon={faCheckToSlot} className="text-5xl text-teal-600 mb-6" />
                         <h2 className='text-4xl font-bold text-slate-800'>99%</h2>
                         <p className='text-teal-400 text-xl'>Success Rate</p>
                     </div>
                     <div className='w-full max-w-[100%] md:max-w-[47%] lg:max-w-[22%] text-center'>
+                        <FontAwesomeIcon icon={faChartPie} className="text-5xl text-teal-600 mb-6" />
                         <h2 className='text-4xl font-bold text-slate-800'>5000k+</h2>
                         <p className='text-teal-400 text-xl'>Complete Orders</p>
                     </div>
