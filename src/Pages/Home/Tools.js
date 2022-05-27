708c0615a8f59e9f3f4842fe7dac30e629aae821
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-27 12:14:04 +0600

    Home page Tools section edited

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -5,7 +5,7 @@ const Tools = () => {
     return (
         <section className='my-12' id='parts'>
             <div className="container mx-auto px-4">
-                <h2 className='text-4xl text-center font-semibold mb-8'>Our T<span className='text-green-400'>oo</span>ls</h2>
+                <h2 className='text-5xl text-center font-semibold mb-8'>Our T<span className='text-green-400'>oo</span>ls</h2>
                 <div className="card-wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     <ToolsCard></ToolsCard>
                 </div>
