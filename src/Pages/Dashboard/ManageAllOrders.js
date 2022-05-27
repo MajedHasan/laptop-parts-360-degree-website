18479dced6691436190bf94f5b042e8349378b67
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-27 20:35:31 +0600

    Manage All Orders page design added

--- src/Pages/Dashboard/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders.js
@@ -3,68 +3,93 @@ import React from 'react';
 const ManageAllOrders = () => {
     return (
         <section className="my-orders">
-            <h2 className="text-3xl text-center font-semibold mb-6">My Orders</h2>
+            <h2 className="text-3xl text-center font-semibold mb-6">All Orders</h2>
             <div className="overflow-x-auto">
                 <table className="table w-full">
                     <thead>
                         <tr>
                             <th>#</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Quantity</th>
+                            <th>Unit Price</th>
+                            <th>Total Price</th>
                             <th>Payment Status</th>
                             <th>Order Status</th>
+                            <th>Order Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                            <td className='text-center'>
+                            <td>Apple Macbook Pro 2017 Battery</td>
+                            <td>15</td>
+                            <td>$<span className='font-semibold text-slate-700'>5.00</span></td>
+                            <td>$<span className='font-semibold text-slate-900'>75.00</span></td>
+                            <td>
                                 <span className="font-semibold text-green-500">paid</span>
                             </td>
                             <td>
                                 <span className='bg-orange-400 rounded-lg py-[4px] px-3 text-white uppercase text-xs font-semibold inline-block'>pending</span>
                             </td>
+                            <td>
+                                <select name="" id="">
+                                    <option value="selectAnOption" className='btn btn-xs bg-red-500 disabled text-white'><small>-- Select An Option --</small></option>
+                                    <option value="ship" className='btn bg-orange-500 text-white'>Ship</option>
+                                    <option value="complete" className='btn bg-green-500 text-white'>Complete</option>
+                                </select>
+                            </td>
                         </tr>
                         <tr>
                             <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                            <td className='text-center'>
+                            <td>Laptop Bag</td>
+                            <td>50</td>
+                            <td>$<span className='font-semibold text-slate-700'>10.00</span></td>
+                            <td>$<span className='font-semibold text-slate-900'>500.00</span></td>
+                            <td>
                                 <span className="text-red-400 font-semibold block">unpaid</span>
-                                <button className='btn btn-xs bg-green-600 border-none text-white block mx-auto'>pay now</button>
-                                <button className='btn btn-xs bg-red-600 border-none text-white'>cancel</button>
                             </td>
                             <td>
                                 <span className='bg-slate-600 rounded-lg py-[4px] px-3 text-white uppercase text-xs font-semibold inline-block'>in progress</span>
                             </td>
+                            <td>
+                                <select name="" id="">
+                                    <option value="selectAnOption" className='btn btn-xs bg-red-500 disabled text-white'><small>-- Select An Option --</small></option>
+                                    <option value="cancel" className='btn bg-red-500 text-white'>Cancel</option>
+                                </select>
+                            </td>
                         </tr>
                         <tr>
                             <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                            <td className='text-center'>
+                            <td>Apple Macbook Pro 2017 Charger</td>
+                            <td>5</td>
+                            <td>$<span className='font-semibold text-slate-700'>20.00</span></td>
+                            <td>$<span className='font-semibold text-slate-900'>100.00</span></td>
+                            <td>
                                 <span className="font-semibold text-green-500">paid</span>
                             </td>
                             <td>
                                 <span className='bg-yellow-400 rounded-lg py-[4px] px-3 text-white uppercase text-xs font-semibold inline-block'>shipped</span>
                             </td>
+                            <td>
+                                <select name="" id="">
+                                    <option value="selectAnOption" className='btn btn-xs bg-red-500 disabled text-white'><small>-- Select An Option --</small></option>
+                                    <option value="complete" className='btn bg-green-500 text-white'>Complete</option>
+                                </select>
+                            </td>
                         </tr>
                         <tr>
                             <th>4</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                            <td className='text-center'>
+                            <td>laptop Mouse</td>
+                            <td>12</td>
+                            <td>$<span className='font-semibold text-slate-700'>10.00</span></td>
+                            <td>$<span className='font-semibold text-slate-900'>120.00</span></td>
+                            <td >
                                 <span className="font-semibold text-green-500">paid</span>
                             </td>
                             <td>
                                 <span className='bg-green-400 rounded-lg py-[4px] px-3 text-white uppercase text-xs font-semibold inline-block'>complete</span>
+                            </td>
+                            <td>
                             </td>
                         </tr>
                     </tbody>
