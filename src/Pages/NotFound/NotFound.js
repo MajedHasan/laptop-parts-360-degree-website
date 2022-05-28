15a33dc155c04df3598b95c758aa33998e71e48e
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-28 08:58:10 +0600

    Not Found Page added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFoundImg from '../../images/3747371.jpg'
 
 const NotFound = () => {
     return (
-        <div>
-
-        </div>
+        <section className="my-8">
+            <div className="container mx-auto px-5">
+                <div className="max-w-2xl w-full shadow rounded p-4 text-center mx-auto">
+                    <img src={notFoundImg} alt="" className='w-full mb-4' />
+                    <h2 className='text-4xl font-bold'>The page your are visited is not found or wrong Link</h2>
+                    <Link to="/" className="text-2xl text-teal-400 font-semibold underline">Please Go to Home Page</Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
