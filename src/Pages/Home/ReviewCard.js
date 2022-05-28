90c71c9cb2ba11053bf544240607b1a79efc3602
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-28 22:41:06 +0600

    Reviews Added with server

--- src/Pages/Home/ReviewCard.js
+++ src/Pages/Home/ReviewCard.js
@@ -7,14 +7,56 @@ const ReviewCard = ({ review }) => {
     return (
         <div className='shadow-md rounded-xl p-6 text-center'>
             <div className='text-center mb-3'>
-                <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
-                <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
-                <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
-                <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
-                <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-700" />
+                {
+                    review?.rating === '1' && <>
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-700" />
+                    </>
+                }
+                {
+                    review?.rating === '2' && <>
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-700" />
+                    </>
+                }
+                {
+                    review?.rating === '3' && <>
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-700" />
+                    </>
+                }
+                {
+                    review?.rating === '4' && <>
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-slate-700" />
+                    </>
+                }
+                {
+                    review?.rating === '5' && <>
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-500" />
+                        <FontAwesomeIcon icon={faStar} className="text-2xl text-orange-700" />
+                    </>
+                }
             </div>
-            <h2 className='text-xl font-bold text-teal-600'>Majed Hasan</h2>
-            <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sequi consequatur minus esse asperiores totam perferendis ea dolores quod voluptas?</p>
+            <h2 className='text-xl font-bold text-teal-600'>{review?.name}</h2>
+            <p >
+                {review?.reviewText}
+            </p>
         </div>
     );
 };
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReviewCard from './ReviewCard';
 
 const Reviews = () => {
+
+    const [reviews, setReviews] = useState([])
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/reviews', {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+            })
+    }, [])
+
+
     return (
         <section className='my-12'>
             <div className="container mx-auto px-4">
                 <h2 className='text-4xl font-bold text-center mb-6'>What <span className="text-teal-600">O</span>ur <span className="text-teal-600">C</span>ustomers Says?</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                    <ReviewCard></ReviewCard>
+                    {
+                        reviews?.slice(0, 6)?.map(review => <ReviewCard
+                            key={review?._id}
+                            review={review}
+                        ></ReviewCard>)
+                    }
+
                 </div>
             </div>
         </section>
