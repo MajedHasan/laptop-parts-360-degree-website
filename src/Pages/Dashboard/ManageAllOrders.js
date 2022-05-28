87da1d3923c5a72014d71e89cc78c0ae65928307
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-28 17:59:49 +0600

    ManageAllOrders page load all order and connect with server

--- src/Pages/Dashboard/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const ManageAllOrders = () => {
+
+    const [allOrders, setAllOrders] = useState([])
+    const [user, loading] = useAuthState(auth)
+
+    useEffect(() => {
+        fetch('http://localhost:5000/orders', {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => setAllOrders(data))
+    }, [])
+
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+
     return (
         <section className="my-orders">
             <h2 className="text-3xl text-center font-semibold mb-6">All Orders</h2>
@@ -19,6 +43,56 @@ const ManageAllOrders = () => {
                         </tr>
                     </thead>
                     <tbody>
+                        {
+                            allOrders.length < 1 ? <>
+                                <tr>
+                                    <td colSpan='8' className='text-2xl text-center font-bold text-red-500'>There have no order</td>
+                                </tr>
+                            </>
+                                :
+                                allOrders.map((order, index) => <>
+                                    <tr>
+                                        <th>{index + 1}</th>
+                                        <td>{order?.partsName}</td>
+                                        <td>{order?.quantity}</td>
+                                        <td>$<span className='font-semibold text-slate-700'>{order?.unitPrice}</span></td>
+                                        <td>$<span className='font-semibold text-slate-900'>{order?.price}</span></td>
+                                        <td>
+                                            {
+                                                order?.paymentStatus === 'paid' && <span className="font-semibold text-green-500">paid</span>
+                                            }
+                                            {
+                                                order?.paymentStatus === 'unpaid' && <span className="text-red-400 font-semibold block">unpaid</span>
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                order?.paymentStatus === 'inProgress' && <span className='bg-slate-600 rounded-lg py-[4px] px-3 text-white uppercase text-xs font-semibold inline-block'>in progress</span>
+                                            }
+                                            {
+                                                order?.paymentStatus === 'pending' && <span className='bg-orange-400 rounded-lg py-[4px] px-3 text-white uppercase text-xs font-semibold inline-block'>pending</span>
+                                            }
+                                            {
+                                                order?.paymentStatus === 'ship' && <span className='bg-yellow-400 rounded-lg py-[4px] px-3 text-white uppercase text-xs font-semibold inline-block'>shipped</span>
+                                            }
+                                            {
+                                                order?.paymentStatus === 'complete' && <span className='bg-green-400 rounded-lg py-[4px] px-3 text-white uppercase text-xs font-semibold inline-block'>complete</span>
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                order?.paymentStatus === 'inProgress' && <button className="btn btn-xs bg-red-500 border-red-500 text-white">Cancel</button>
+                                            }
+                                            {
+                                                order?.paymentStatus === 'pending' && <button className="btn btn-xs bg-yellow-500 border-yellow-500 text-white">Ship</button>
+                                            }
+                                            {
+                                                order?.paymentStatus === 'ship' && <button className="btn btn-xs bg-green-500 border-green-500 text-white">Complete</button>
+                                            }
+                                        </td>
+                                    </tr>
+                                </>)
+                        }
                         <tr>
                             <th>1</th>
                             <td>Apple Macbook Pro 2017 Battery</td>
