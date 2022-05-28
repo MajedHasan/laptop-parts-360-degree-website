6e116e94fcdc22f58cdb4327b8e06eea78514f0e
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-28 22:51:16 +0600

    Tools Card Added

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ToolsCard from './ToolsCard';
 
 const Tools = () => {
+
+    const [tools, setTools] = useState([])
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/parts', {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setTools(data)
+            })
+    }, [])
+
+
     return (
         <section className='my-12' id='parts'>
             <div className="container mx-auto px-4">
                 <h2 className='text-5xl text-center font-semibold mb-8'>Our T<span className='text-green-400'>oo</span>ls</h2>
                 <div className="card-wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <ToolsCard></ToolsCard>
+                    {
+                        tools?.slice(0, 6)?.map(tool => <ToolsCard
+                            key={tool?._id}
+                            tool={tool}
+                        ></ToolsCard>)
+                    }
                 </div>
             </div>
         </section>
--- src/Pages/Home/ToolsCard.js
+++ src/Pages/Home/ToolsCard.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ToolsCard = ({ tools }) => {
+const ToolsCard = ({ tool }) => {
 
-    // const { _id, img, name, price, minOrderQTY, quantity, description } = tools
+    const { _id, img, name, price, minOrderQuantity, quantity, description } = tool
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-                <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" className="rounded-xl" />
+                <img src={img} alt="Shoes" className="rounded-xl" />
             </figure>
             <div className="card-body items-center">
-                <h2 className="card-title text-3xl">Name</h2>
-                <h3 className='text-orange-400 text-2xl font-bold'>Price: $20</h3>
-                <p><small>Min order Quantity: <span className='font-bold'>5</span></small></p>
-                <p><small>Available Quantity: <span className='font-bold'>50</span></small></p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptatem in odio magnam cum, est non aperiam eos corrupti tempora similique veritatis id nostrum ipsa.</p>
+                <h2 className="card-title text-3xl">{name}</h2>
+                <h3 className='text-orange-400 text-2xl font-bold'>Price: ${price}</h3>
+                <p><small>Min order Quantity: <span className='font-bold'>{minOrderQuantity}</span></small></p>
+                <p><small>Available Quantity: <span className='font-bold'>{quantity}</span></small></p>
+                <p>{description?.length > 60 ? `${description.slice(0, 60)}...` : description}</p>
                 <div className="card-actions">
-                    <Link to={`/purchase/:id`} className="btn btn-primary bg-green-400 border-green-400 text-white">Book Now</Link>
+                    <Link to={`/purchase/${_id}`} className="btn btn-primary bg-green-400 border-green-400 text-white">Book Now</Link>
                 </div>
             </div>
         </div>
