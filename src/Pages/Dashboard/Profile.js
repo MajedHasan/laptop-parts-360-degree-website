6dc04ff9f663bdabac88f676b96eb4477bed9fb3
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-28 16:35:38 +0600

    Profile page setup with server

--- src/Pages/Dashboard/Profile.js
+++ src/Pages/Dashboard/Profile.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
+import { toast } from 'react-toastify';
 
 const Profile = () => {
 
@@ -13,7 +14,7 @@ const Profile = () => {
 
 
     useEffect(() => {
-        const url = `http://localhost:5000/profile/${user?.email}`
+        const url = `http://localhost:5000/user/${user?.email}`
         fetch(url, {
             method: "GET",
             headers: {
@@ -22,12 +23,14 @@ const Profile = () => {
         })
             .then(res => res.json())
             .then(data => {
-                setEducation(data.education)
-                setLocation(data.location)
-                setPhoneNumber(data.phone)
-                setLinkedinProfileLink(data.linkedin)
+                if (data?.education && data?.location && data?.phone && data?.linkedin) {
+                    setEducation(data?.education || '')
+                    setLocation(data?.location || '')
+                    setPhoneNumber(data?.phone || '')
+                    setLinkedinProfileLink(data?.linkedin || '')
+                }
             })
-    }, [])
+    }, [user])
 
 
     if (loading) {
@@ -37,17 +40,13 @@ const Profile = () => {
 
     const handleUpdateProfile = async (event) => {
         event.preventDefault()
-        const education = event.target.education.value
-        const location = event.target.location.value
-        const phone = event.target.phone.value
-        const linkedin = event.target.linkedin.value
         const profile = {
             education: education,
             location: location,
-            phone: phone,
-            linkedin: linkedin
+            phone: phoneNumber,
+            linkedin: linkedinProfileLink
         }
-        const url = `http://localhost:5000/profile`
+        const url = `http://localhost:5000/user/${user?.email}`
         await fetch(url, {
             method: "PUT",
             headers: {
@@ -58,7 +57,9 @@ const Profile = () => {
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                if (data?.result?.modifiedCount === 1 || data?.result?.upsertedCount === 1) {
+                    toast.success("Profile Updated Successfully")
+                }
             })
     }
 
@@ -88,13 +89,13 @@ const Profile = () => {
             <div className="container mx-auto px-5">
                 <h2 className='mb-8 text-center text-3xl text-teal-800'>My Profile</h2>
                 <div className="shadow rounded p-5 max-w-md w-full mx-auto">
-                    <form onClick={handleUpdateProfile}>
-                        <input type="text" className="input input-bordered w-full mb-4" value={user.displayName} disabled />
-                        <input type="email" className="input input-bordered w-full mb-4" value={user.email} disabled />
-                        <input type="text" placeholder='Education' name='education' onChange={handleEducation} className="input input-bordered w-full mb-4" value={education} />
-                        <input type="text" placeholder='Location' name='location' onChange={handleLocation} className="input input-bordered w-full mb-4" value={location} />
-                        <input type="text" placeholder='phone Number' name='phone' onChange={handlePhone} className="input input-bordered w-full mb-4" value={phoneNumber} />
-                        <input type="text" placeholder='Linkedin Profile Link' name='linkedin' onChange={handleLinkedin} className="input input-bordered w-full mb-4" value={linkedinProfileLink} />
+                    <form onSubmit={handleUpdateProfile}>
+                        <input type="text" className="input input-bordered w-full mb-4" value={user?.displayName} disabled />
+                        <input type="email" className="input input-bordered w-full mb-4" value={user?.email} disabled />
+                        <input type="text" placeholder='Education' name='education' onChange={handleEducation} className="input input-bordered w-full mb-4" value={education || ''} />
+                        <input type="text" placeholder='Location' name='location' onChange={handleLocation} className="input input-bordered w-full mb-4" value={location || ''} />
+                        <input type="text" placeholder='phone Number' name='phone' onChange={handlePhone} className="input input-bordered w-full mb-4" value={phoneNumber || ''} />
+                        <input type="text" placeholder='Linkedin Profile Link' name='linkedin' onChange={handleLinkedin} className="input input-bordered w-full mb-4" value={linkedinProfileLink || ''} />
                         <button type="submit" className='btn bg-green-400 border-green-500 text-white block mx-auto'>Update</button>
                     </form>
                 </div>
