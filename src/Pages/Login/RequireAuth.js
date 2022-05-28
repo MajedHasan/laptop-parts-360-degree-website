e79a1930fbb78f144aec118f5716d8971235cb02
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-28 15:47:14 +0600

    JSON web token added

--- src/Pages/Login/RequireAuth.js
+++ src/Pages/Login/RequireAuth.js
@@ -1,18 +1,20 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
+import useToken from '../../hooks/useToken';
 import Loading from '../Shared/Loading';
 
 const RequireAuth = ({ children }) => {
 
     const location = useLocation()
     const [user, loading] = useAuthState(auth)
+    const [token] = useToken(user)
 
     if (loading) {
         return <Loading></Loading>
     }
 
-    if (!user) {
+    if (!token) {
         return <Navigate to='/login' state={{ from: location }} replace></Navigate>
     }
 
